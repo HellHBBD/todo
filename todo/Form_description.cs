a4cc9f193a5781adc1b16cc278e650d157de4574
Peter <x1023179802587@gmail.com>
2025-01-05 00:46:54 +0800

    b

--- todo/Form_description.cs
+++ todo/Form_description.cs
@@ -6,13 +6,8 @@ namespace todo
 {
     public partial class Form_description : Form
     {
-        private Task currentTask; // 當前任務
+        private Task currentTask;
         private TextBox txtName;
-        private DateTimePicker dtpDate;
-        private NumericUpDown numImportance;
-        private TextBox txtDescription;
-        private Button btnSave;
-        private Button btnCancel;
 
         public Form_description(Task curtask)
         {
@@ -42,95 +37,19 @@ namespace todo
                 Text = currentTask.name
             };
 
-            Label lblDate = new Label
-            {
-                Text = "任務日期：",
-                Location = new Point(20, 60),
-                AutoSize = true
-            };
-            dtpDate = new DateTimePicker
-            {
-                Location = new Point(100, 60),
-                Value = currentTask.date,
-                Format = DateTimePickerFormat.Short
-            };
-
-            Label lblImportance = new Label
-            {
-                Text = "重要性：",
-                Location = new Point(20, 100),
-                AutoSize = true
-            };
-            numImportance = new NumericUpDown
-            {
-                Location = new Point(100, 100),
-                Minimum = 1,
-                Maximum = 10,
-                Value = currentTask.important,
-                Width = 50
-            };
-
             Label lblDescription = new Label
             {
                 Text = "任務描述：",
                 Location = new Point(20, 140),
                 AutoSize = true
             };
-            txtDescription = new TextBox
-            {
-                Location = new Point(100, 140),
-                Width = 250,
-                Height = 60,
-                Multiline = true,
-                Text = currentTask.description
-            };
 
-            btnSave = new Button
-            {
-                Text = "保存",
-                Location = new Point(100, 220),
-                Width = 80
-            };
-            btnSave.Click += BtnSave_Click;
-
-            btnCancel = new Button
-            {
-                Text = "取消",
-                Location = new Point(200, 220),
-                Width = 80
-            };
-            btnCancel.Click += BtnCancel_Click;
 
             this.Controls.Add(lblName);
             this.Controls.Add(txtName);
-            this.Controls.Add(lblDate);
-            this.Controls.Add(dtpDate);
-            this.Controls.Add(lblImportance);
-            this.Controls.Add(numImportance);
-            this.Controls.Add(lblDescription);
-            this.Controls.Add(txtDescription);
-            this.Controls.Add(btnSave);
-            this.Controls.Add(btnCancel);
         }
 
-        private void BtnSave_Click(object sender, EventArgs e)
-        {
-            // 保存修改
-            currentTask.name = txtName.Text;
-            currentTask.date = dtpDate.Value;
-            currentTask.important = (int)numImportance.Value;
-            currentTask.description = txtDescription.Text;
 
-            // 關閉視窗
-            this.DialogResult = DialogResult.OK;
-            this.Close();
-        }
 
-        private void BtnCancel_Click(object sender, EventArgs e)
-        {
-            // 不保存修改，直接關閉
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
-        }
     }
 }
