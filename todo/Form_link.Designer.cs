011f40ef2dad0647e9532daa9997d15a0fbbf48c
Hell HBBD <paulyu602@gmail.com>
2025-01-10 04:50:55 +0800

    add cycle check

--- todo/Form_link.Designer.cs
+++ todo/Form_link.Designer.cs
@@ -29,32 +29,24 @@
         private void InitializeComponent()
         {
             listBox_prev = new ListBox();
-            button_confirm = new Button();
             button_cancel = new Button();
             listBox_available = new ListBox();
             button_right = new Button();
             button_left = new Button();
             listBox_next = new ListBox();
+            label_prev = new Label();
+            label_next = new Label();
+            label_available = new Label();
             SuspendLayout();
             // 
             // listBox_prev
             // 
             listBox_prev.FormattingEnabled = true;
-            listBox_prev.Location = new Point(12, 12);
+            listBox_prev.Location = new Point(12, 35);
             listBox_prev.Name = "listBox_prev";
             listBox_prev.Size = new Size(230, 303);
             listBox_prev.TabIndex = 1;
             listBox_prev.SelectedIndexChanged += listBox_prev_SelectedIndexChanged;
-            // 
-            // button_confirm
-            // 
-            button_confirm.Location = new Point(549, 452);
-            button_confirm.Name = "button_confirm";
-            button_confirm.Size = new Size(112, 34);
-            button_confirm.TabIndex = 2;
-            button_confirm.Text = "確定";
-            button_confirm.UseVisualStyleBackColor = true;
-            button_confirm.Click += button_confirm_Click;
             // 
             // button_cancel
             // 
@@ -62,14 +54,14 @@
             button_cancel.Name = "button_cancel";
             button_cancel.Size = new Size(112, 34);
             button_cancel.TabIndex = 3;
-            button_cancel.Text = "取消";
+            button_cancel.Text = "完成";
             button_cancel.UseVisualStyleBackColor = true;
             button_cancel.Click += button_cancel_Click;
             // 
             // listBox_available
             // 
             listBox_available.FormattingEnabled = true;
-            listBox_available.Location = new Point(284, 12);
+            listBox_available.Location = new Point(284, 35);
             listBox_available.Name = "listBox_available";
             listBox_available.Size = new Size(230, 303);
             listBox_available.TabIndex = 4;
@@ -98,37 +90,69 @@
             // listBox_next
             // 
             listBox_next.FormattingEnabled = true;
-            listBox_next.Location = new Point(552, 12);
+            listBox_next.Location = new Point(552, 35);
             listBox_next.Name = "listBox_next";
             listBox_next.Size = new Size(230, 303);
             listBox_next.TabIndex = 7;
             listBox_next.SelectedIndexChanged += listBox_next_SelectedIndexChanged;
+            // 
+            // label_prev
+            // 
+            label_prev.AutoSize = true;
+            label_prev.Location = new Point(82, 9);
+            label_prev.Name = "label_prev";
+            label_prev.Size = new Size(82, 23);
+            label_prev.TabIndex = 8;
+            label_prev.Text = "previous";
+            // 
+            // label_next
+            // 
+            label_next.AutoSize = true;
+            label_next.Location = new Point(641, 9);
+            label_next.Name = "label_next";
+            label_next.Size = new Size(47, 23);
+            label_next.TabIndex = 9;
+            label_next.Text = "next";
+            // 
+            // label_available
+            // 
+            label_available.AutoSize = true;
+            label_available.Location = new Point(367, 9);
+            label_available.Name = "label_available";
+            label_available.Size = new Size(64, 23);
+            label_available.TabIndex = 10;
+            label_available.Text = "未加入";
             // 
             // Form_link
             // 
             AutoScaleDimensions = new SizeF(11F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(794, 498);
+            Controls.Add(label_available);
+            Controls.Add(label_next);
+            Controls.Add(label_prev);
             Controls.Add(listBox_next);
             Controls.Add(button_left);
             Controls.Add(button_right);
             Controls.Add(listBox_available);
             Controls.Add(button_cancel);
-            Controls.Add(button_confirm);
             Controls.Add(listBox_prev);
             Name = "Form_link";
             Text = "Form_link";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ListBox listBox_prev;
-        private Button button_confirm;
         private Button button_cancel;
         private ListBox listBox_available;
         private Button button_right;
         private Button button_left;
         private ListBox listBox_next;
+        private Label label_prev;
+        private Label label_next;
+        private Label label_available;
     }
 }
--- todo/Form_link.cs
+++ todo/Form_link.cs
@@ -14,14 +14,16 @@ namespace todo
     public partial class Form_link : Form
     {
         User currentUser = Program.currentuser;
-        HashSet<string> prev_temp = new HashSet<string>();
-        HashSet<string> next_temp = new HashSet<string>();
+        HashSet<string> prev_temp;
+        HashSet<string> next_temp;
         string currentTask = "";
         //LinkStatus status;
         public Form_link(string currentTask)
         {
             InitializeComponent();
             this.currentTask = currentTask;
+            prev_temp = currentUser.taskList[currentTask].prev;
+            next_temp = currentUser.taskList[currentTask].next;
 
             foreach (string item in currentUser.taskList.Keys)
             {
@@ -46,24 +48,84 @@ namespace todo
             }
         }
 
+        bool checkP(HashSet<string> record, string start)
+        {
+            foreach (string item in currentUser.taskList[start].prev)
+            {
+                if (record.Contains(item))
+                {
+                    return true;
+                }
+                record.Add(item);
+                if (checkP(record, item))
+                {
+                    return true;
+                }
+                record.Remove(item);
+            }
+            return false;
+        }
+
+        /*bool checkPrevCycle(string start)*/
+        /*{*/
+        /*    HashSet<string> record = new HashSet<string>();*/
+        /*    return checkP(record, start);*/
+        /*}*/
+
+        bool checkN(HashSet<string> record, string start)
+        {
+            foreach (string item in currentUser.taskList[start].next)
+            {
+                if (record.Contains(item))
+                {
+                    return true;
+                }
+                record.Add(item);
+                if (checkN(record, item))
+                {
+                    return true;
+                }
+                record.Remove(item);
+            }
+            return false;
+        }
+
+        /*bool checkNextCycle(string start)*/
+        /*{*/
+        /*    HashSet<string> record = new HashSet<string>();*/
+        /*    return checkN(record, start);*/
+        /*}*/
+
         private void button_left_Click(object sender, EventArgs e)
         {
             string? task = null;
             /* available -> prev */
             if (listBox_available.SelectedItem != null && (task = listBox_available.SelectedItem.ToString()) != null)
             {
-                listBox_available.Items.Remove(task);
-
                 prev_temp.Add(task);
-                listBox_prev.Items.Add(task);
-                listBox_prev.SelectedItem = task;
+
+                HashSet<string> record = new HashSet<string>();
+                record.Add(currentTask);
+                if (checkP(record, task))
+                {
+                    prev_temp.Remove(task);
+                    MessageBox.Show("先後關係出現循環", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    currentUser.taskList[task].next.Add(currentTask);
+
+                    listBox_prev.Items.Add(task);
+                    listBox_prev.SelectedItem = task;
+
+                    listBox_available.Items.Remove(task);
+                }
             }
             /* next -> available */
             else if (listBox_next.SelectedItem != null && (task = listBox_next.SelectedItem.ToString()) != null)
             {
-                // TODO check cycle
-
                 next_temp.Remove(task);
+                currentUser.taskList[task].prev.Remove(currentTask);
                 listBox_next.Items.Remove(task);
 
                 listBox_available.Items.Add(task);
@@ -78,6 +140,7 @@ namespace todo
             if (listBox_prev.SelectedItem != null && (task = listBox_prev.SelectedItem.ToString()) != null)
             {
                 prev_temp.Remove(task);
+                currentUser.taskList[task].next.Remove(currentTask);
                 listBox_prev.Items.Remove(task);
 
                 listBox_available.Items.Add(task);
@@ -86,29 +149,24 @@ namespace todo
             /* available -> next */
             else if (listBox_available.SelectedItem != null && (task = listBox_available.SelectedItem.ToString()) != null)
             {
-                // TODO check cycle
-
-                listBox_available.Items.Remove(task);
-
                 next_temp.Add(task);
-                listBox_next.Items.Add(task);
-                listBox_next.SelectedItem = task;
-            }
-        }
+                HashSet<string> record = new HashSet<string>();
+                record.Add(currentTask);
+                if (checkN(record, task))
+                {
+                    next_temp.Remove(task);
+                    MessageBox.Show("先後關係出現循環", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    currentUser.taskList[task].prev.Add(currentTask);
 
-        private void button_confirm_Click(object sender, EventArgs e)
-        {
-            currentUser.taskList[currentTask].prev = prev_temp;
-            foreach (string task in prev_temp)
-            {
-                currentUser.taskList[task].next.Add(currentTask);
+                    listBox_available.Items.Remove(task);
+
+                    listBox_next.Items.Add(task);
+                    listBox_next.SelectedItem = task;
+                }
             }
-            currentUser.taskList[currentTask].next = next_temp;
-            foreach (string task in next_temp)
-            {
-                currentUser.taskList[task].prev.Add(currentTask);
-            }
-            Close();
         }
 
         private void button_cancel_Click(object sender, EventArgs e)
